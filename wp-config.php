29b5f0fc13f780809100c5725de581e7f8d1bd09
Gott50 <xxGott50xx@gmail.com>
2018-03-17 12:57:23 +0100

    enable local updating via WordPress

--- wp-config.php
+++ wp-config.php
@@ -87,3 +87,7 @@ if ( !defined('ABSPATH') )
 
 /** Sets up WordPress vars and included files. */
 require_once(ABSPATH . 'wp-settings.php');
+
+
+/** enable local updating via WordPress */
+define('FS_METHOD', 'direct');
