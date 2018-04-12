bb5171df61542e2ca369b993341db4761f0f9a2e
Gott50 <xxGott50xx@gmail.com>
2018-04-12 13:40:40 +0200

    update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -26,10 +26,10 @@ define('DB_NAME', 'wordpress');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'examplepass');
 
 /** MySQL hostname */
-define('DB_HOST', 'db:3306');
+define('DB_HOST', 'mysql');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
